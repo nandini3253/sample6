3f1566dae1426780d29c54ab79e45bca04a6b27f
nandini <nandini3253@gmail.com>
2024-12-11 14:37:51 +0530

    fifth commit

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,6 +1,6 @@
 public class PrimeNumber{
     public static void main(String Args[]){
-        System.out.println("Prime numbers between 1 , 100");
+        System.out.println("Prime numbers between 1 and 100 are:");
         for (int num = 2; num <= 100; num++) {
             boolean isPrime = true;
             for (int i = 2; i <= Math.sqrt(num); i++) {
